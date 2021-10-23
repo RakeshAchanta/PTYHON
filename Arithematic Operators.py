f06739b41ca1a6d1c92b6029b78e5a7712879298
RakeshAchanta <92287545+RakeshAchanta@users.noreply.github.com>
2021-10-23 17:38:33 +0530

    Add files via upload

---
+++ Arithematic Operators.py
@@ -0,0 +1,17 @@
+#ARTHEMATIC OPERATOR
+A=10
+B=20
+#Addition
+print("SUM OF", A, "AND", B, "IS", A+B)
+#Subtraction
+print("DIFFERENCE OF", A ,"AND", B , "IS", A-B)
+#Multiplication
+print("PRODUCT OF", A, "AND", B, "IS",A*B)
+#Division
+print("QUOTIENT WHEN",A,"IS DIVIDED BY",B,"IS",A/B)
+#Floor Division
+print("FLOOR QUOTIENT WHEN",A, "IS DIVIDED BY",B,"IS",A//B)
+#Modulo Division
+print("REMAINDER WHEN",A,"DIVIEDED",B,"IS",A%B)
+#Exponentiation
+print("VALUE OF",A,"TO THE POWER",B,"IS",A**B)
