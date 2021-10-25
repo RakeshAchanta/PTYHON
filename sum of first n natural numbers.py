c66914f031120cffe06d81add10e0d4b71cf07f7
RakeshAchanta <92287545+RakeshAchanta@users.noreply.github.com>
2021-10-25 17:12:26 +0530

    Add files via upload

---
+++ sum of first n natural numbers.py
@@ -0,0 +1,13 @@
+n=1000
+s=n*(n+1)//2
+print('SUM OF FIRST', n, 'NATURAL NUMBERS IS',s)
+
+n=int(input())
+s=n*(n+1)//2
+print('Sum of First', n, 'Natural numbers is:', s)
+
+n=int(input('Enter a number:'))
+s=n*(n+1)//2
+print('Sum of First', n, 'Natural numbers is:', s)
+
+
