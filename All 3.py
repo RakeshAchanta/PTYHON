4c91dd6d58902301813938a3d6710b60c7b5b187
RakeshAchanta <92287545+RakeshAchanta@users.noreply.github.com>
2021-10-26 16:14:42 +0530

    Add files via upload

---
+++ All 3.py
@@ -0,0 +1,9 @@
+n=int(input('ENTER A NUMBER:'))
+p=n*(n+1)//2
+q=n*(n+1)
+r=n**2
+s=p+q+r
+print("Sum of first", n,"Natural numbers is", p)
+print("Sum of first", n,"Even Natural numbers is", q)
+print("Sum of first", n,"Odd Natural numbers is", r)
+print("Total sum of first", n,"Natural numbers,Even Natural numbers and Odd Natural numbers is:",s)
