b10ee2c3a52e607ee8d51013fe4614ed6267a5f1
RakeshAchanta <92287545+RakeshAchanta@users.noreply.github.com>
2021-10-27 17:23:02 +0530

    Add files via upload

---
+++ Comparision or Relational operators.py
@@ -0,0 +1,21 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  4 2021, 13:27:16) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # < Less than
+>>> # <= Less than or equal to
+>>> # > Greater than
+>>> # >= Greater than or equal to
+>>> # == Equal to
+>>> # != Not equal to
+>>> 11<2
+False
+>>> 11<=2
+False
+>>> 11>2
+True
+>>> 11>=2
+True
+>>> 11==2
+False
+>>> 11!=2
+True
+>>> 
