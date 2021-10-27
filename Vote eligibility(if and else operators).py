2efa8e5b098baf5af5322a55a8871f90f172be96
RakeshAchanta <92287545+RakeshAchanta@users.noreply.github.com>
2021-10-27 17:42:15 +0530

    Add files via upload

---
+++ Vote eligibility(if and else operators).py
@@ -0,0 +1,9 @@
+#Desion Making
+#Taking age of a person and checking if they are eligible to vote or not
+age = int(input("ENTER THE AGE OF THE PERSON:"))
+# syntax for if - 
+if age>18:
+    print("YOU CAN VOTE")
+else:
+    print("YOU ARE NOT ELIGILE TO VOTE")
+
