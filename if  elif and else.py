68cbdb159fb3a8117875bed0681f31b8f31db91b
RakeshAchanta <92287545+RakeshAchanta@users.noreply.github.com>
2021-10-28 16:54:58 +0530

    Add files via upload

---
+++ if  elif and else.py
@@ -0,0 +1,9 @@
+#TO Check a num is positive or negative
+num=int(input("ENTER THE NUMBER:"))
+if num<0:
+    print(num, "IS A NEGATIVE NUMBER")
+elif num>0:
+        print(num, "IS A POSITIVE NUMBER")
+else:
+    print(num, "IS NOT EITHER POSITIVE OR NEGATIVE")
+    
