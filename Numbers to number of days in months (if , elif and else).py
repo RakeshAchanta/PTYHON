5fbd7fc1556b5c2ac79b9e84a70efd4770b8bfc3
RakeshAchanta <92287545+RakeshAchanta@users.noreply.github.com>
2021-10-30 17:03:29 +0530

    Add files via upload

---
+++ Numbers to number of days in months (if , elif and else).py
@@ -0,0 +1,28 @@
+n=int(input("ENTER NUMBER OF THE MONTH:"))
+if n==1:
+    print("MONTH 1 HAS 31 DAYS")
+elif n==2:
+    print("MONTH 2 HAS 28 DAYS")    
+elif n==3:
+    print("MONTH 3 HAS 31 DAYS")
+elif n==4:
+    print("MONTH 4 HAS 30 DAYS")
+elif n==5:
+    print("MONTH 5 HAS 31 DAYS")
+elif n==6:
+    print("MONTH 6 HAS 30 DAYS")
+elif n==7:
+    print("MONTH 7 HAS 31 DAYS")
+elif n==8:
+    print("MONTH 8 HAS 31 DAYS")
+elif n==9:
+    print("MONTH 9 HAS 30 DAYS")
+elif n==10:
+    print("MONTH 10 HAS 31 DAYS")
+elif n==11:
+    print("MONTH 11 HAS 30 DAYS")
+elif n==12:
+    print("MONTH 12 HAS 31 DAYS")
+else:
+    print("ENTER A VALID MONTH NUMBER")    
+    
